8d8ac0c959bba077c6991012a63f47d4385682cc
Kauan Soares Mota <4kauanmota@gmail.com>
2023-07-28 11:18:18 -0300

    feat: app.js configuration

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
+import { useState } from 'react';
+import { StyleSheet, View, Pressable, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalList from './components/GoalList';
 
 export default function App() {
+  const [allGoals, setAllGoals] = useState([]);
+  const [inputModal, setInputModal] = useState(false);
+
+  const modelGoalInputHandler = () => {
+    setInputModal(!inputModal);
+  }
+
+  const addGoalHandler = (newGoal) => {
+    setAllGoals((prev) => prev.concat({text: newGoal, id: Math.random().toString()}));
+  }
+
+  const deleteGoal = (id, name) => {
+    setAllGoals(allGoals.filter(el => el.id != id))
+    console.log(name);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light'/>
+      <View style={styles.container}>
+        <Pressable onPress={modelGoalInputHandler} style={styles.addNewGoalButton}>
+          <Text style={styles.addNewGoalButtonText}> ADD NEW GOAL </Text>
+        </Pressable>
+        
+        <GoalInput addGoalHandler={addGoalHandler} inputModal={inputModal} modelGoalInputHandler={modelGoalInputHandler} />
+  
+        <GoalList allGoals={allGoals} deleteGoal={deleteGoal} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 25,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#202124',
   },
+
+  addNewGoalButton: {
+    backgroundColor: '#1e90ff',
+    height: 55,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, 
+
+  addNewGoalButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
 });
